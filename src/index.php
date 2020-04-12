e43a78081856068a95101f977033ed3db404a66f
leiluspocus <laila.atrmouh@gmail.com>
2020-04-12 20:21:54 +0200

    :art: add some front end code

--- src/index.php
+++ src/index.php
@@ -1,5 +1,39 @@
-<h1>
+<button id="cache">Cacher le titre</button>
+<h1 id="titre">
     <?php 
         echo 'Bonjour, monde !';
     ?>
 </h1>
+
+<h1>Mes fruits préférés</h1>
+<ul>
+<?php 
+    $fruits = ['pomme', 'banane', 'poire', 'fraise'];
+    foreach ( $fruits as $fruit ) {
+        echo '<li>' . $fruit . '</li>';
+    }
+?>
+</ul>
+
+<script>
+document.getElementById('cache').addEventListener('click', () => {
+    var display_title = document.getElementById('titre').style.display;
+    if ( display_title == 'block' || display_title === '' ) {
+        document.getElementById('titre').style.display = 'none';
+        document.getElementById('cache').innerHTML = 'Montrer le titre';
+    }
+    else {
+        document.getElementById('titre').style.display = 'block';
+        document.getElementById('cache').innerHTML = 'Cacher le titre';
+    }
+});
+</script>
+<style>
+    h1 {
+        background-color: cyan;
+    }
+
+    #titre {
+        display: block;
+    } 
+</style>
